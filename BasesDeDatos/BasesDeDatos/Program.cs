5ce4ff56327f2cce8fac75cb6bff8059d112f61f
Julian0816 <83423059+Julian0816@users.noreply.github.com>
2022-04-14 01:35:56 +0100

    first commit

---
+++ BasesDeDatos/BasesDeDatos/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace BasesDeDatos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Para seleccionar un tema:
+            //Des-comentar el tema que se quier usar.
+
+            //Ahora mismo solo puedo usar un tema a la vez, la pregunta es como puedo cambiar de usar un solo tema, o dos a la vez
+            //o tres.. o todos los temas a la vez?
+
+            //Temario Tema1 = new Temario();
+            //Tema1.LlamarQuestionario1();
+
+            //Temario Tema2 = new Temario();
+            //Tema2.LlamarQuestionario2();
+
+            //Temario Tema3 = new Temario();
+            //Tema3.LlamarQuestionario3();
+
+            //Temario Tema4 = new Temario();
+            //Tema4.LlamarQuestionario4();
+
+            //Temario Temma5 = new Temario();
+            //Temma5.LlamarQuestionario5();
+
+            //Temario Tema6 = new Temario();
+            //Tema6.LlamarQuestionario6();
+
+            //Temario Tema7 = new Temario();
+            //Tema7.LlamarQuestionario7();
+
+            //Temario Tema8 = new Temario();
+            //Tema8.LlamarQuestionario8();
+
+            //Temario Tema9 = new Temario();
+            //Tema9.LlamarQuestionario9();
+
+            //Temario Tema10 = new Temario();
+            //Tema10.LlamarQuestionario10();
+
+            //Temario Tema11 = new Temario();
+            //Tema11.LlamarQuestionario11();
+
+            //Temario Tema12 = new Temario();
+            //Tema12.LlamarQuestionario12();
+
+            //Temario Tema13 = new Temario();
+            //Tema13.LlamarQuestionario13();
+
+            //Temario Tema14 = new Temario();
+            //Tema14.LlamarQuestionario14();
+
+        }
+    }
+}
+
