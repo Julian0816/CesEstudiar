15574862ed22ed8166600a33312d5ce782743f27
Julian Martinez <julian.martinez.cs@gmail.com>
2022-04-20 18:57:22 -0500

    Colapce todos los metodos para que el codigo se vea mas ordenado, y mas facil de entender. ya se pueden empezar a llenar la preguntas del tema de programacion a partir del tema 2

--- BasesDeDatos/BasesDeDatos/TemaProgramacion.cs
+++ BasesDeDatos/BasesDeDatos/TemaProgramacion.cs
@@ -5,16 +5,16 @@ namespace BasesDeDatos
     {
         public void LlamarQuestionario1()
         {
-            string[] tema = {"Un archivo se expresa en bytes. Verdadero",
-                "Un sistema de informacion se corresponde de datos, harware y software",
-                "Monotorizacion, Recuperacion, Administracion son funciones del SGBD",
-                "Es Redis una base de datos? si!",
-                "Un archivo se caracteriza por un nombre y una ruta. Si!",
-                "Los sistemas de informacion ayudan a recuperar informacion relevante. Verdadero",
-                "Son sistemas de DB relacional: MySQL, MariaDB, SQLite / No relacionales: MongoDB",
-                "El objetivo de un sistema de informacion es producir informacion valida para la posterior toma de decisiones. Verdadero",
-                "Ventajas de un SGBD: Integridad, Seguridad, Eficiencia - Desventajas: Rapidez",
-                "Es un SGBD es seguro? Si"};
+            string[] tema = {"En la programacion, la secuencia de la instrucciones - define el flujo del programa",
+                "Una asignacion es una intruccion que le da un valor numerico a una variable - Falso",
+                "Que podemos concluir de esta instruccion? MiVar = 5 - Se trata de una asignacion",
+                "El tipo de datos string representa una cadena de caracteres - Verdadero",
+                "Un metodo o funcion... - No existe limitacion a los parametros de entrada",
+                "Cual de la siguientes afirmaciones se aplica a las variables globales - Son variables por al menos todo el script donde son declaradas",
+                "Un parametro de entrada solo es visible en el metodo que lo define - Verdadero",
+                "El predicado logico siguiente </br>(! Verdadero && varBool) vale... Siempre falso",
+                "Que es un float en programacion? - Un tipo de tados simple",
+                "En programacion no se permite mezclar variables de distinto tipos de datos - Verdadero"};
 
             for (int i = 0; i < tema.Length; i++)
             {
@@ -38,6 +38,321 @@ namespace BasesDeDatos
                 Console.WriteLine("Vuelve a revisar los apuntes");
                
 
+            }
+        }
+        public void LlamarQuestionario2()
+        {
+            string[] tema = {"",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                ""};
+
+            for (int i = 0; i < tema.Length; i++)
+            {
+                Random r = new Random();
+                int index = r.Next(tema.Length);
+                Console.WriteLine($"Pregunta: {tema[index]}");
+                break;
+            }
+            Console.WriteLine("-------------");
+            Console.WriteLine("got it? (y/n)");
+            string respuesta = Console.ReadLine();
+            Console.WriteLine("-------------");
+
+
+            if (respuesta == "y")
+            {
+                LlamarQuestionario2();
+            }
+            else
+            {
+                Console.WriteLine("Vuelve a revisar los apuntes");
+            }
+        }
+        public void LlamarQuestionario3()
+        {
+            string[] tema = {"",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                ""};
+
+            for (int i = 0; i < tema.Length; i++)
+            {
+                Random r = new Random();
+                int index = r.Next(tema.Length);
+                Console.WriteLine($"Pregunta: {tema[index]}");
+                break;
+            }
+            Console.WriteLine("-------------");
+            Console.WriteLine("got it? (y/n)");
+            string respuesta = Console.ReadLine();
+            Console.WriteLine("-------------");
+
+
+            if (respuesta == "y")
+            {
+                LlamarQuestionario3();
+            }
+            else
+            {
+                Console.WriteLine("Vuelve a revisar los apuntes");
+            }
+        }
+        public void LlamarQuestionario4()
+        {
+            string[] tema = {"",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                ""};
+
+            for (int i = 0; i < tema.Length; i++)
+            {
+                Random r = new Random();
+                int index = r.Next(tema.Length);
+                Console.WriteLine($"Pregunta: {tema[index]}");
+                break;
+            }
+            Console.WriteLine("-------------");
+            Console.WriteLine("got it? (y/n)");
+            string respuesta = Console.ReadLine();
+            Console.WriteLine("-------------");
+
+
+            if (respuesta == "y")
+            {
+                LlamarQuestionario4();
+            }
+            else
+            {
+                Console.WriteLine("Vuelve a revisar los apuntes");
+            }
+        }
+        public void LlamarQuestionario5()
+        {
+            string[] tema = {"",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                ""};
+
+            for (int i = 0; i < tema.Length; i++)
+            {
+                Random r = new Random();
+                int index = r.Next(tema.Length);
+                Console.WriteLine($"Pregunta: {tema[index]}");
+                break;
+            }
+            Console.WriteLine("-------------");
+            Console.WriteLine("got it? (y/n)");
+            string respuesta = Console.ReadLine();
+            Console.WriteLine("-------------");
+
+
+            if (respuesta == "y")
+            {
+                LlamarQuestionario5();
+            }
+            else
+            {
+                Console.WriteLine("Vuelve a revisar los apuntes");
+            }
+        }
+        public void LlamarQuestionario6()
+        {
+            string[] tema = {"",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                ""};
+
+            for (int i = 0; i < tema.Length; i++)
+            {
+                Random r = new Random();
+                int index = r.Next(tema.Length);
+                Console.WriteLine($"Pregunta: {tema[index]}");
+                break;
+            }
+            Console.WriteLine("-------------");
+            Console.WriteLine("got it? (y/n)");
+            string respuesta = Console.ReadLine();
+            Console.WriteLine("-------------");
+
+
+            if (respuesta == "y")
+            {
+                LlamarQuestionario6();
+            }
+            else
+            {
+                Console.WriteLine("Vuelve a revisar los apuntes");
+            }
+        }
+        public void LlamarQuestionario7()
+        {
+            string[] tema = {"",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                ""};
+
+            for (int i = 0; i < tema.Length; i++)
+            {
+                Random r = new Random();
+                int index = r.Next(tema.Length);
+                Console.WriteLine($"Pregunta: {tema[index]}");
+                break;
+            }
+            Console.WriteLine("-------------");
+            Console.WriteLine("got it? (y/n)");
+            string respuesta = Console.ReadLine();
+            Console.WriteLine("-------------");
+
+
+            if (respuesta == "y")
+            {
+                LlamarQuestionario7();
+            }
+            else
+            {
+                Console.WriteLine("Vuelve a revisar los apuntes");
+            }
+        }
+        public void LlamarQuestionario8()
+        {
+            string[] tema = {"",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                ""};
+
+            for (int i = 0; i < tema.Length; i++)
+            {
+                Random r = new Random();
+                int index = r.Next(tema.Length);
+                Console.WriteLine($"Pregunta: {tema[index]}");
+                break;
+            }
+            Console.WriteLine("-------------");
+            Console.WriteLine("got it? (y/n)");
+            string respuesta = Console.ReadLine();
+            Console.WriteLine("-------------");
+
+
+            if (respuesta == "y")
+            {
+                LlamarQuestionario8();
+            }
+            else
+            {
+                Console.WriteLine("Vuelve a revisar los apuntes");
+            }
+        }
+        public void LlamarQuestionario9()
+        {
+            string[] tema = {"",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                ""};
+
+            for (int i = 0; i < tema.Length; i++)
+            {
+                Random r = new Random();
+                int index = r.Next(tema.Length);
+                Console.WriteLine($"Pregunta: {tema[index]}");
+                break;
+            }
+            Console.WriteLine("-------------");
+            Console.WriteLine("got it? (y/n)");
+            string respuesta = Console.ReadLine();
+            Console.WriteLine("-------------");
+
+
+            if (respuesta == "y")
+            {
+                LlamarQuestionario9();
+            }
+            else
+            {
+                Console.WriteLine("Vuelve a revisar los apuntes");
+            }
+        }
+        public void LlamarQuestionario10()
+        {
+            string[] tema = {"",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                ""};
+
+            for (int i = 0; i < tema.Length; i++)
+            {
+                Random r = new Random();
+                int index = r.Next(tema.Length);
+                Console.WriteLine($"Pregunta: {tema[index]}");
+                break;
+            }
+            Console.WriteLine("-------------");
+            Console.WriteLine("got it? (y/n)");
+            string respuesta = Console.ReadLine();
+            Console.WriteLine("-------------");
+
+
+            if (respuesta == "y")
+            {
+                LlamarQuestionario10();
+            }
+            else
+            {
+                Console.WriteLine("Vuelve a revisar los apuntes");
             }
         }
     }
